bf6d25217571e5064d3455b1da12dda98ea36d69
Puja <singh.puja92@hotmail.com>
2013-08-19 21:26:07 +0530

    Update DebugViewHelper.php

--- Classes/TYPO3/Base/ViewHelpers/DebugViewHelper.php
+++ Classes/TYPO3/Base/ViewHelpers/DebugViewHelper.php
@@ -2,7 +2,7 @@
 namespace TYPO3\Base\ViewHelpers;
 
 /*                                                                        *
- * This script belongs to the TYPO3 Flow package "Fluid".                 *
+ * This script belongs to the TYPO3 package "Base".          	          *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
@@ -13,7 +13,7 @@ namespace TYPO3\Base\ViewHelpers;
 
 
 /**
- * View helper that outputs its child nodes with \TYPO3\Flow\var_dump()
+ * View helper that outputs its child nodes with var_dump()
  *
  * = Examples =
  *
@@ -42,7 +42,7 @@ namespace TYPO3\Base\ViewHelpers;
  *
  * @api
  */
-class DebugViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+class DebugViewHelper extends \TYPO3\Base\Core\ViewHelper\AbstractViewHelper {
 
 	/**
 	 * @var boolean
@@ -50,23 +50,22 @@ class DebugViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 	protected $escapingInterceptorEnabled = FALSE;
 
 	/**
-	 * Wrapper for \TYPO3\Flow\var_dump()
+	 * A wrapper for var_dump() or Doctrine's Dump Utility.
 	 *
-	 * @param string $title
-	 * @param boolean $typeOnly Whether only the type should be returned instead of the whole chain.
-	 * @return string debug string
+	 * @param string $title optional custom title for the debug output
+	 * @param integer $maxDepth Sets the max recursion depth of the dump (defaults to 8). De- or increase the number according to your needs and memory limit.
+	 * @param boolean $plainText If TRUE, the dump is in plain text, if FALSE the debug output is in HTML format.
+	 * @param boolean $ansiColors If TRUE, ANSI color codes is added to the plaintext output, if FALSE (default) the plaintext debug output not colored.
+	 * @param boolean $inline if TRUE, the dump is rendered at the position of the <f:debug> tag. If FALSE (default), the dump is displayed at the top of the page.
+	 * @param array $blacklistedClassNames An array of class names (RegEx) to be filtered. Default is an array of some common class names.
+	 * @param array $blacklistedPropertyNames An array of property names and/or array keys (RegEx) to be filtered. Default is an array of some common property names.
+	 * @return string
 	 */
-	public function render($title = NULL, $typeOnly = FALSE) {
-		$expressionToExamine = $this->renderChildren();
-		if ($typeOnly === TRUE && $expressionToExamine !== NULL) {
-			$expressionToExamine = (is_object($expressionToExamine) ? get_class($expressionToExamine) : gettype($expressionToExamine));
+	public function render($title = NULL, $maxDepth = 8, $plainText = FALSE, $ansiColors = FALSE, $inline = FALSE, $blacklistedClassNames = NULL, $blacklistedPropertyNames = NULL) {
+		if (class_exists('Doctrine\\Common\\Util\\Debug')) {
+			return \Doctrine\Common\Util\Debug::dump($this->renderChildren(), $maxDepth, !$plainText);
 		}
-
-		ob_start();
-		\TYPO3\Flow\var_dump($expressionToExamine, $title);
-		$output = ob_get_contents();
-		ob_end_clean();
-		return $output;
+		return var_export($this->renderChildren(), TRUE);
 	}
 }
 
